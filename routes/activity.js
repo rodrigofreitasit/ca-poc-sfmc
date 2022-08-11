6afbf1f3d9397202e1c6b9cab51f24a1d526d8d5
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2022-08-11 13:33:10 -0300

    add function textToCopy

--- routes/activity.js
+++ routes/activity.js
@@ -27,36 +27,18 @@ function logData(req) {
         secure: req.secure,
         originalUrl: req.originalUrl
     });
-    
-    console.log("body: " + util.inspect(req.body));
-    console.log("headers: " + util.inspect(req.headers));
-    console.log("trailers: " + req.trailers);
-    console.log("method: " + req.method);
-    console.log("url: " + req.url);
-    console.log("params: " + util.inspect(req.params));
-    console.log("query: " + util.inspect(req.query));
-    console.log("route: " + req.route);
-    console.log("cookies: " + req.cookies);
-    console.log("ip: " + req.ip);
-    console.log("path: " + req.path);
-    console.log("host: " + req.host);
-    console.log("fresh: " + req.fresh);
-    console.log("stale: " + req.stale);
-    console.log("protocol: " + req.protocol);
-    console.log("secure: " + req.secure);
-    console.log("originalUrl: " + req.originalUrl);
 }
 
 exports.edit = function (req, res) {
     console.log('edit request');
     // logData(req);
-    res.send(200, 'Edit');
+    res.status(200).send('Edit');
 };
 
 exports.save = function (req, res) {
     console.log('save request');
     // logData(req);
-    res.send(200, 'Save');
+    res.status(200).send('Save');
 };
 
 exports.execute = function (req, res) {
@@ -69,8 +51,8 @@ exports.execute = function (req, res) {
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             var decodedArgs = decoded.inArguments[0];
-            console.log('inArguments', JSON.stringify(decoded.inArguments));
-            console.log('decodedArgs', JSON.stringify(decodedArgs));
+            // console.log('inArguments', JSON.stringify(decoded.inArguments));
+            // console.log('decodedArgs', JSON.stringify(decodedArgs));
 
             const headers = {
                 'Content-Type': 'application/json',
@@ -85,7 +67,7 @@ exports.execute = function (req, res) {
                     console.error(`ERROR ${err}`)
                 })
         
-            res.send(200, 'Execute');
+            res.status(200).send('Execute')
         } else {
             console.error('inArguments invalid.');
             return res.status(400).end();
@@ -96,11 +78,11 @@ exports.execute = function (req, res) {
 exports.publish = function (req, res) {
     console.log('publish request');
     // logData(req);
-    res.send(200, 'Publish');
+    res.status(200).send('Publish')
 };
 
 exports.validate = function (req, res) {
     console.log('validate request');
     // logData(req);
-    res.send(200, 'Validate');
+    res.status(200).send('Validate')
 };
