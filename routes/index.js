2440e4b12fa86a51bbef48b329a1c7974d3956ba
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2022-08-23 18:23:06 -0300

    teste

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var activity = require('./activity');
-
+var GetTokens = require('../public/js/customActivity')
 exports.index = function (req, res) {
     console.log('index request!');
+    console.log('GetTokens: ', GetTokens)
 
 
     // if (!req.session.token) {
